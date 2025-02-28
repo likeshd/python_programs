ea8a94fa021685747707db3e3c564412e5ced29b
LikeshD <likesh.dahake@assettl.com>
2025-02-28 17:11:43 +0530

    added bisic codes

--- simple_2.py
+++ simple_2.py
@@ -36,13 +36,42 @@
 # print(d)
 
 ##Fibonacci Series
-fib = [0,1]
-n = 5
-for i in range(5):
-    fib.append(fib[-1]+ fib[-2])
-print(fib)
-v=",".join(str(e) for e in fib)
-print(v)
+# fib = [0,1]
+# n = 5
+# for i in range(5):
+#     fib.append(fib[-1]+ fib[-2])
+# print(fib)
+# v=",".join(str(e) for e in fib)
+# print(v)
+
+##the Max number in List
+# l1 = [4,56,78,2,5,48,69,94,7,8]
+# max = 0
+# for num in l1:
+#     if num > max:
+#         max = num
+# print(max)
+
+## Finding middle element in list
+# l1 = [4,56,78,2,5,48,69,94,7,8]
+# l2 = sorted(l1)
+# print(l2)
+# m = len(l2)/2
+# print(m)
+# print(l2[int(m)])
+
+##Coverting list into a string
+
+# s = "P Y H T O N"
+# x = s.split(" ")
+# print(x)
+# n = "".join(x)
+# print(n)
+# print(n.split())
+
+## counting spaces in string
+st = "P r ogram in g"
+print(st.count(" "))
 
 
 
